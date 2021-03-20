4fa46cfdf0698e0fee4743d3221dca07868f6fd0
Matt Calsada <mcalsada@atlas-sys.com>
2021-03-20 09:42:17 -0400

    Log js to client log. Add readme

--- InternetArchiveSearch.lua
+++ InternetArchiveSearch.lua
@@ -9,8 +9,6 @@
 --
 -- InternetArchiveSearch.lua does search of Internet Archive for the LoanTitle for loans.
 -- autoSearch (boolean) determines whether the search is performed automatically when a request is opened or not.
---
--- "query" is the text box name on archive.org
 
 local autoSearch = GetSetting("AutoSearch");
 
@@ -20,27 +18,25 @@ local addonForm = {};
 function Init()
 	if GetFieldValue("Transaction", "RequestType") == "Loan" then
 
-interfaceMngr = GetInterfaceManager();
-		
 		interfaceMngr = GetInterfaceManager();
-		
+
 		-- Create a form
 		addonForm.Form = interfaceMngr:CreateForm("Internet Archive Search", "Internet Archive Search");
-		
+
 		-- Add a browser
 		addonForm.Browser = addonForm.Form:CreateBrowser("Internet Archive Search", "Internet Archive Search", "Internet Archive", "Chromium");
-		
+
 		-- Hide the text label
 		addonForm.Browser.TextVisible = false;
 		addonForm.Browser:CollapseTextPlaceholder();
-		
+
 		-- Since we didn't create a ribbon explicitly before creating our browser, it will have created one using the name we passed the CreateBrowser method.  
 		-- We can retrieve that one and add our buttons to it.
 		addonForm.RibbonPage = addonForm.Form:GetRibbonPage("Internet Archive");
-		
+
 		-- Create the search button
 		addonForm.RibbonPage:CreateButton("Search", GetClientImage("Search32"), "Search", "Internet Archive Search");
-		
+
 		-- After we add all of our buttons and form elements, we can show the form.
 		addonForm.Form:Show();
 
@@ -52,7 +48,7 @@ end
 
 function Search()
 	addonForm.Browser:RegisterPageHandler("formExists", "searchform","SearchFormLoaded", false);
-	addonForm.Browser:Navigate("http://archive.org");	
+	addonForm.Browser:Navigate("http://archive.org");
 end
 
 function SearchFormLoaded()	
@@ -66,29 +62,31 @@ function SearchInternetArchives(browser, formName, inputName, value)
 		--Script to execute
 		local searchInternetArchives = [[
 			(function(formName, inputName, value) {
-				
+				//Write to the client log. 
+				//Use ES6 Template Strings to concatenate - using back-ticks: https://developers.google.com/web/updates/2015/01/ES6-Template-Strings
+				atlasAddonAsync.executeAddonFunction("LogDebug", `Performing Internet Archives Search: ${value}` );
+
 				let form = document.forms[formName];
 				if (!(form)) {
-					console.log('Unable to find the form');
+					atlasAddonAsync.executeAddonFunction("LogDebug", `Unable to find form: ${formName}` );
 					return;
 				}
 				
 				let inputElement = form.elements[inputName];
 				if (!(inputElement)) {
-					console.log('Unable to find the input');
+					atlasAddonAsync.executeAddonFunction("LogDebug", `Unable to find form input: ${inputName}` );
 					return;
 				}
 				inputElement.value = value;
 
-				//Internet Archive masks the form submit with a hidden element
-				//Instead try to find the button and click it
+				//Internet Archive masks form submit with a hidden element with name "submit"
+				//Instead, look for a the button and click it
 				if (form.getElementsByTagName("button").length == 1) {
-					console.log('Only 1 to submit');
 					form.getElementsByTagName("button")[0].click();
 				}
 			})
 		]];
-				
+
 		browser:ExecuteScript(searchInternetArchives, { formName, inputName, value });
 	end
 end
